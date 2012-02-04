d5494210dc68db0408d97d467d3f5bf8d88a9382
Mr Tiggr <Mr.Tiggr@gmail.com>
2012-02-04 18:28:26 +1100

    fixed viewws

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var app = require('express').createServer();
-
+app.configure(function() {
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
 app.get('/', function(req, res) {
-  res.render('/home/ubuntu/NodeGIS/views/index.ejs', {
-    user: 'tiggr'
+  res.render(__dirname + '/views/index.ejs', {
+    user: 'tiggr',
+    layout: false
   });
 });
 
