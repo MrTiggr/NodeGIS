3a79363929e56c58d1c16b8c3eb4e034e5806de5
Mr Tiggr <Mr.Tiggr@gmail.com>
2012-02-04 22:02:48 +1100

    db dammit

--- server.js
+++ server.js
@@ -4,61 +4,65 @@ var redis = require("redis");
 var jsts = require(__dirname + "/lib/jsts.js");
 var gis = {
   DataManager: function DataManager(opts) {
+    var self = this;
     this.client = redis.createClient();
     this.client.on("error", function(err) {
       console.log("Error " + err);
     });
-
     this.listSources = function(callback) {
-      this.client.hgetall("ngis::sources", function(err, obj) {
+      self.client.hgetall("ngis::sources", function(err, obj) {
         var sources = [];
         for (var s in obj) {
           sources.push(obj[s]);
         }
         callback(sources);
       });
+      this.close = function() {
+        self.client.close();
+      }
+    }, DataSource: function DataSource(id, schema, opts) {
+      this.id = id;
+      this.schema = schema;
+      this.query = function(query, callback) {
+
+      };
+      this.getById = function(id, callback) {
+
+      };
+      this.insert = function(feature, callback) {
+
+      };
+      this.update = function(feature, callback) {
+
+      };
+    },
+    Feature: function Feature(opts) {
+
     }
-  },
-  DataSource: function DataSource(id, schema, opts) {
-    this.id = id;
-    this.schema = schema;
-    this.query = function(query, callback) {
-
-    };
-    this.getById = function(id, callback) {
-
-    };
-    this.insert = function(feature, callback) {
-
-    };
-    this.update = function(feature, callback) {
-
-    };
-  },
-  Feature: function Feature(opts) {
-
   }
-}
-var DB = new gis.DataManager({});
 
-app.configure(function() {
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }));
-});
 
-app.get('/', function(req, res) {
-
-  res.render(__dirname + '/views/index.ejs', {
-    user: 'tiggr',
-    sources: DB.listSources(),
-    layout: false
+  app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
   });
-});
 
-app.listen(3000);
+  app.get('/', function(req, res) {
+    var DB = new gis.DataManager({});
+    DB.listSources(function(srs) {}
+    res.render(__dirname + '/views/index.ejs', {
+      user: 'tiggr',
+      sources: srs,
+      layout: false
+    });
+    }
+    DB.close();
+  });
+
+  app.listen(3000);
