5bf3bf6ce4d92f05eed545c475d68304e980bebc
Mr Tiggr <Mr.Tiggr@gmail.com>
2012-02-04 21:56:27 +1100

    db

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 var express = require('express');
 var app = require('express').createServer();
 var redis = require("redis");
-var gis = require(__dirname + "/lib/nodeGIS.js");
-var DB = new gis.DataManager();
+var jsts = require(__dirname + "/lib/jsts.js");
+var gis = {
+  DataManager: function DataManager(opts) {
+    this.client = redis.createClient();
+    client.on("error", function(err) {
+      console.log("Error " + err);
+    });
+
+    this.listSources = function(callback) {
+      this.client.hgetall("ngis::sources", function(err, obj) {
+        var sources = [];
+        for (var s in obj) {
+          sources.push(obj[s]);
+        }
+        callback(sources);
+      });
+    }
+  },
+  DataSource: function DataSource(id, schema, opts) {
+    this.id = id;
+    this.schema = schema;
+    this.query = function(query, callback) {
+
+    };
+    this.getById = function(id, callback) {
+
+    };
+    this.insert = function(feature, callback) {
+
+    };
+    this.update = function(feature, callback) {
+
+    };
+  },
+  Feature: function Feature(opts) {
+
+  }
+}
+var DB = new gis.DataManager({});
 
 app.configure(function() {
   app.use(express.methodOverride());
