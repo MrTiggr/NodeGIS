ca8733782c14de5fb9ea2394142f55bf436869a2
Mr Tiggr <Mr.Tiggr@gmail.com>
2012-02-05 14:44:48 +1100

    objects/params

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = require('express').createServer();
+var app = require('express').createServer(express.logger(), express.bodyParser());
 var redis = require("redis");
 var jsts = require(__dirname + "/lib/jsts.js");
 var uuid = require('node-uuid');
@@ -118,6 +118,7 @@ app.post('/addSource', function(req, res) {
   var source = new gis.DataSource(req.param("sourceid"), req.param("sourcename"));
   source.id = req.param("sourceid");
   source.name = req.param("sourcename");
+  console.log(source);
   DB.addSource(source, function(srs) {
     DB.listSources(function(srses) {
       res.render(__dirname + '/views/sources.ejs', {
